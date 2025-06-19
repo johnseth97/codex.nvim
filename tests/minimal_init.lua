7194efd3fe59810c41cf3529592c431de5d7bee4
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-06-19 02:41:42 -0400

    Wrapping in pcall

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,5 +1,5 @@
 vim.cmd 'set rtp+=.'
 vim.cmd 'set rtp+=./plenary.nvim' -- if using as a submodule or symlinked
-require 'plugin.codex' -- triggers plugin/gh_dash.lua
+pcall(require, 'plugin.codex') -- triggers plugin/gh_dash.lua
 vim.opt.runtimepath:append(vim.fn.getcwd())
 vim.opt.runtimepath:append(vim.fn.stdpath 'data' .. '/site/pack/deps/start/plenary.nvim')
